8bcda54dfd3cdcbdbd1115232e4709f1fe24cbb9
kevin_xie <xie.kevin.j@gmail.com>
2020-07-14 22:57:44 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/dog'
+require 'pry'
+class Dog
+    def bark
+        puts "Woof!"
+    def sit 
+        puts "The Dog is sitting"
+    end
+    end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+require_relative '../lib/person'
+class Person
+    def talk
+        puts "Hello World!"
+        def walk
+           puts "The Person is walking"
+        end
+    end
+end
